d509981d4698dc77fe1afd9ad5b409f060e1de47
Alan James Rager <guy@books.lickmy.rocks>
2015-02-12 02:50:34 -0800

    updating apa102.lua to have more comments and to take out the singleton logic on the init function

--- apa102.lua
+++ apa102.lua
@@ -1,14 +1,26 @@
-apa102 = {APA102_INITIALIZED = false}
+--[[
+APA102 control helpers
+
+Use these to push pixels to apa102 (dotstar) lights over SPI on the nodemcu platform.
+
+Alan Rager
+Feb 11, 2015
+--]]
+
+apa102 = {}
 	
+--Initialize for writing spi
 function apa102.init()
-	if (apa102.APA102_INITIALIZED == false) then
-		pin=8 
-		spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
-		gpio.mode(pin, gpio.OUTPUT)
-		apa102.APA102_INITIALIZED = true
-	end
+	-- Thanks, elevation: http://www.esp8266.com/viewtopic.php?p=8959#p8959
+	-- Posted Feb 04, 2015, lifted Feb 11, 2015
+	spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
 end
 
+--[[
+	Write out a string of bytes to the apa102 lights
+	@param number brightness - a single byte of data to write; 3 bits high, then 5 bits of brightness to set globally
+	@param string data - a string of bytes in bgr order to write to the light strip
+--]]
 function apa102.write(brightness, data)
 	local led_count = string.len(data) / 3
 	local output_buffer = {}
