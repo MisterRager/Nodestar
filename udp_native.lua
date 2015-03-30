2e0795d374474f19d92159f5d89ae8ca060d8a66
Alan Rager <Alan.Rager@gmail.com>
2015-03-29 18:45:39 -0700

    changes for udp_native.lua to match c++ interfaces and add brightness control

--- udp_native.lua
+++ udp_native.lua
@@ -3,7 +3,7 @@ n_udp_server = {
     output = nil,
     message = nil,
     port = 65506,
-    brightness = 31, --max 31, min 0
+    brightness = 5, --max 31, min 0
 }
 n_udp_server.__index = n_udp_server
 
@@ -15,12 +15,13 @@ end
 
 function n_udp_server:packet_callback(data)
     --print(self.message)
-    spi.apa102_send(1, data)
+    spi.apa102_send(data)
 end
 
 function n_udp_server:start()
     print("Starting UDP nodestar server")
     spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
+    spi.apa102_brightness( self.brightness )
 
     svr=net.createServer(net.UDP)   
     svr:on("receive", function(socket, data)
@@ -47,4 +48,9 @@ function n_udp_server:process_loop()
         self:packet_callback(out)
         tmr.wdclr()
     end
+end
+
+function n_udp_server:set_brightness( brt )
+    self.brightness = brt
+    return spi.api102_brightness( brt )
 end
